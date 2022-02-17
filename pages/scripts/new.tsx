be9ef51830bd3047d579c4246c53fc6d8b3e537c
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-02-18 06:43:07 +0800

    Improve Result UI

--- pages/scripts/new.tsx
+++ pages/scripts/new.tsx
@@ -4,6 +4,7 @@ import Layout from '../../components/layout'
 import { CardanoSerializationLib } from '../../cardano/serialization-lib'
 import type { Cardano } from '../../cardano/serialization-lib'
 import { Buffer } from 'buffer'
+import Link from 'next/link'
 
 const NewScript: NextPage = () => {
   const [addresses, setAddresses] = useState<Set<string>>(new Set())
@@ -42,6 +43,14 @@ const getKeyHash = (cardano: Cardano, address: string): string => {
   const bytes = cardano.getBech32AddressKeyHash(address).to_bytes()
   return toHex(bytes)
 }
+const getAllScriptAddress = (cardano: Cardano, addresses: Set<string>): string => {
+  const publicKeyScripts = Array.from(addresses, (address) => {
+    const keyHash = cardano.getBech32AddressKeyHash(address)
+    return cardano.buildPublicKeyScript(keyHash)
+  })
+  const allScript = cardano.buildAllScript(publicKeyScripts)
+  return cardano.getScriptBech32Address(allScript, false)
+}
 
 type ResultProps = {
   addresses: Set<string>
@@ -49,26 +58,32 @@ type ResultProps = {
 }
 
 function Result({ addresses, cardano }: ResultProps) {
+  const script = addresses.size > 1 && getAllScriptAddress(cardano, addresses)
   return (
-    <div>
-      <div className='shadow rounded-md mb-2'>
-        <div className='px-3 py-1 border-b'>
-          <div className='rounded-sm'>
-            <select className='px-2 py-1 text-sm rounded-sm w-20'>
-              <option value="all">All</option>
-            </select>
-          </div>
+    <div className='shadow border rounded-md mb-2'>
+      <div className='flex p-3 border-b bg-gray-100'>
+        {!script && <p className='grow text-gray-400'>Need more than 1 addresses</p>}
+        {script && (
+          <p className='grow'>
+            <Link href={`/scripts/${script}`}><a>{script}</a></Link>
+          </p>
+        )}
+        <div className='flex border border-blue-600 rounded-sm bg-blue-600 text-white'>
+          <select className='px-2 py-1 bg-transparent'>
+            <option value="all">All</option>
+          </select>
+          <div className='px-2'>of {addresses.size}</div>
         </div>
-        <ul className='divide-y'>
-          {Array.from(addresses, (address) => (
-            <li className='px-2 py-1' key={address}>
-              <p>{address}</p>
-              <p className='text-sm text-gray-400'>{getKeyHash(cardano, address)}</p>
-            </li>
-          ))}
-        </ul>
       </div>
-    </div >
+      <ul className='divide-y text-sm'>
+        {Array.from(addresses, (address) => (
+          <li className='p-3' key={address}>
+            <p>{address}</p>
+            <p className='text-gray-400'>{getKeyHash(cardano, address)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
@@ -101,8 +116,8 @@ function AddAddress({ cardano, onAdd }: AddAddressProps) {
   }
 
   return (
-    <div className='shadow mb-2'>
-      <div className='px-4 py-5 bg-white sm:p-6'>
+    <div className='shadow border rounded-md mb-2'>
+      <div className='px-4 py-5 sm:p-6'>
         <textarea className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full border border-gray-300 rounded-md p-2' onChange={onChange} rows={5} value={value} placeholder="Address"></textarea>
         {isError && <p className='text-sm py-1 text-red-400'>Invalid address</p>}
         {!isError && <p className='text-sm py-1 text-gray-400'>{keyHash}</p>}
