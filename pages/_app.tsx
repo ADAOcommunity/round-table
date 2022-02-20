db5d272baa3b5ad6413f96c4a2f0db6baf03985d
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-02-20 13:11:46 +0800

    Setup GraphQL client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState } from 'react'
 import { NetworkContext } from '../components/network-switch'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const networkState = useState(true)
+  const [isMainnet, setMainnet] = useState(true)
+
+  const network = isMainnet ? 'mainnet' : 'testnet'
+  const graphQLURI = `https://graphql-api.${network}.dandelion.link/`
+  const apolloClient = new ApolloClient({
+    uri: graphQLURI,
+    cache: new InMemoryCache()
+  })
 
   return (
-    <NetworkContext.Provider value={networkState}>
-      <Component {...pageProps} />
+    <NetworkContext.Provider value={[isMainnet, setMainnet]}>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </NetworkContext.Provider>
   )
 }
--- pages/scripts/[script].tsx
+++ pages/scripts/[script].tsx
@@ -1,11 +1,81 @@
+import { gql, useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import Layout from '../../components/layout'
+
+const UTxOsQuery = gql`
+query UTxOsByAddress($address: String!) {
+  utxos(
+    where: {
+      address: {
+        _eq: $address
+      }
+    }
+  ) {
+    txHash
+    index
+    value
+    tokens {
+      asset {
+        policyId
+        assetName
+        name
+        ticker
+        decimals
+      }
+      quantity
+    }
+  }
+}
+`
+
+type Asset = {
+  policyId: string
+  assetName: string
+  name: string
+  ticker: string
+  decimals: number
+}
+
+type Token = {
+  asset: Asset
+  quantity: string
+}
+
+type UTxO = {
+  txHash: string
+  index: number
+  value: string
+  tokens: Token[]
+}
+
+type QueryData = {
+  utxos: UTxO[]
+}
+
+type QueryVars = {
+  address: string
+}
+
+const lovelaceValueOf = (utxos: UTxO[]): number => {
+  return utxos.map(({ value }) => parseInt(value)).reduce((p, c) => p + c)
+}
 
 const Script: NextPage = () => {
   const router = useRouter()
   const { script } = router.query
+  const { loading, error, data } = useQuery<QueryData, QueryVars>(UTxOsQuery, {
+    variables: { address: script as string }
+  })
 
-  return <p>Script: {script}</p>
+  return (
+    <Layout>
+      <div className='p-4 rounded-md bg-white'>
+        <h1 className='font-medium text-center'>{script}</h1>
+        <h2 className='font-medium text-center'>{data && lovelaceValueOf(data.utxos) / 1e6}&nbsp;₳</h2>
+      </div>
+    </Layout>
+  )
 }
 
 export default Script
