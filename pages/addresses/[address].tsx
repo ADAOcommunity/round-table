eef1952416e2909301f864eb22846264b686431a
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-02-25 11:22:00 +0800

    Add asset inputs

--- pages/addresses/[address].tsx
+++ pages/addresses/[address].tsx
@@ -164,6 +164,12 @@ const GetAddress: NextPage = () => {
   const { address } = router.query
   const [config, _] = useContext(ConfigContext)
   const { loading, error, balance } = useBalanceQuery(address as string, config)
+  type AssetInput = {
+    id: string,
+    quantity: bigint
+    max: bigint
+  }
+  const [assetInputs, setAssetInputs] = useState<AssetInput[]>([])
 
   if (loading) return (
     <div className='text-center'>
@@ -186,6 +192,12 @@ const GetAddress: NextPage = () => {
     return decoder.decode(buffer)
   }
 
+  const setAssetInput = (newAssetInput: AssetInput) => {
+    setAssetInputs(assetInputs.map((oldAssetInput) => {
+      return oldAssetInput.id === newAssetInput.id ? newAssetInput : oldAssetInput
+    }))
+  }
+
   if (balance) {
     return (
       <Layout>
@@ -201,22 +213,41 @@ const GetAddress: NextPage = () => {
               <input className='p-2 block w-full outline-none' placeholder='0.000000' />
               <span className='p-2 bg-gray-200'>₳</span>
             </label>
+            {assetInputs.map(({ id, quantity, max }) => (
+              <label key={id} className='flex block border rounded-md overflow-hidden'>
+                <input
+                  onChange={(e) => setAssetInput({ id, max, quantity: BigInt(e.target.value) })}
+                  className='p-2 block w-full outline-none'
+                  type="number"
+                  step={1}
+                  min={1}
+                  max={max.toString()}
+                  value={quantity.toString()}
+                />
+                <span className='p-2 bg-gray-200'>{getAssetName(id.slice(56))}</span>
+              </label>
+            ))}
             <div className='relative'>
               <button className='block rounded-md bg-gray-200 p-2 peer'>Add Asset</button>
               <ul className='absolute mt-1 divide-y bg-white text-sm max-h-64 rounded-md shadow overflow-y-scroll invisible peer-focus:visible hover:visible'>
-                {Array.from(balance.assets, ([id, quantity]) => (
-                  <li key={id}>
-                    <button className='block w-full h-full px-1 py-2 hover:bg-slate-100'>
-                      <div className='flex space-x-2'>
-                        <span>{getAssetName(id.slice(56))}</span>
-                        <span className='grow text-right'>{quantity.toString()}</span>
-                      </div>
-                      <div className='flex space-x-1'>
-                        <span className='font-mono text-gray-500 text-xs'>{id.slice(0, 56)}</span>
-                      </div>
-                    </button>
-                  </li>
-                ))}
+                {Array.from(balance.assets)
+                  .filter(([id, _]) => !assetInputs.find((asset) => asset.id === id))
+                  .map(([id, quantity]) => (
+                    <li key={id}>
+                      <button
+                        onClick={() => setAssetInputs(assetInputs.concat({ id, max: quantity, quantity }))}
+                        className='block w-full h-full px-1 py-2 hover:bg-slate-100'
+                      >
+                        <div className='flex space-x-2'>
+                          <span>{getAssetName(id.slice(56))}</span>
+                          <span className='grow text-right'>{quantity.toString()}</span>
+                        </div>
+                        <div className='flex space-x-1'>
+                          <span className='font-mono text-gray-500 text-xs'>{id.slice(0, 56)}</span>
+                        </div>
+                      </button>
+                    </li>
+                  ))}
               </ul>
             </div>
           </div>
