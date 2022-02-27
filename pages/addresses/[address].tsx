05e8a7fd9e0c09643bf9b210bdde435d757b3a25
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-02-27 09:25:13 +0800

    Add multiple recipients

--- pages/addresses/[address].tsx
+++ pages/addresses/[address].tsx
@@ -56,21 +56,22 @@ type TxOutput = {
 }
 
 type Balance = {
-  txOutputs: TxOutput[]
   lovelace: bigint
   assets: AssetBalance
 }
 
+type AddressBalance = { txOutputs: TxOutput[] } & Balance
+
 type BalanceQuery = {
   loading: boolean
   error: boolean
-  balance?: Balance
+  addressBalance?: AddressBalance
 }
 
 const useBalanceQuery = (address: string, config: Config): BalanceQuery => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
-  const [balance, setBalance] = useState<Balance | undefined>(undefined)
+  const [addressBalance, setAddressBalance] = useState<AddressBalance | undefined>(undefined)
 
   useEffect(() => {
     let isMounted = true
@@ -100,7 +101,7 @@ const useBalanceQuery = (address: string, config: Config): BalanceQuery => {
             })
           })
 
-          isMounted && utxos && setBalance({
+          isMounted && utxos && setAddressBalance({
             txOutputs: utxos.map(({ txHash, index }) => { return { txHash, index } }),
             lovelace: utxos.map(({ value }) => BigInt(value)).reduce((acc, v) => acc + v, BigInt(0)),
             assets
@@ -144,7 +145,7 @@ const useBalanceQuery = (address: string, config: Config): BalanceQuery => {
               })
             })
 
-            isMounted && info && setBalance({
+            isMounted && info && setAddressBalance({
               txOutputs: info.utxo_set.map(({ tx_hash, tx_index }) => {
                 return { txHash: tx_hash, index: tx_index }
               }),
@@ -165,29 +166,24 @@ const useBalanceQuery = (address: string, config: Config): BalanceQuery => {
     }
   }, [address, config])
 
-  return { loading, error, balance }
+  return { loading, error, addressBalance }
 }
 
 const GetAddress: NextPage = () => {
   const router = useRouter()
   const { address } = router.query
   const [config, _] = useContext(ConfigContext)
-  const { loading, error, balance } = useBalanceQuery(address as string, config)
-  type AssetInput = {
-    id: string,
-    quantity: bigint
-    max: bigint
+  const { loading, error, addressBalance } = useBalanceQuery(address as string, config)
+
+  type Recipient = { address: string } & Balance
+
+  const defaultRecipient = {
+    address: '',
+    lovelace: BigInt(1e6),
+    assets: new Map()
   }
-  const [lovelaceInput, setLovelaceInput] = useState(BigInt(1e6))
-  const [assetInputs, setAssetInputs] = useState<AssetInput[]>([])
 
-  if (loading) return (
-    <div className='text-center'>
-      Loading...
-    </div>
-  )
-
-  if (error) return <div>An error happened.</div>
+  const [recipients, setRecipients] = useState<Recipient[]>([defaultRecipient])
 
   const getAssetName = (assetName: string): string => {
     const buffer = Buffer.from(assetName, 'hex')
@@ -195,69 +191,113 @@ const GetAddress: NextPage = () => {
     return decoder.decode(buffer)
   }
 
-  const setAssetInput = (newAssetInput: AssetInput) => {
-    setAssetInputs(assetInputs.map((oldAssetInput) => {
-      return oldAssetInput.id === newAssetInput.id ? newAssetInput : oldAssetInput
-    }))
+  const toADA = (lovelace: bigint) => toDecimal(lovelace, 6)
+
+  const Recipient = (recipient: Recipient, index: number, balance: Balance) => {
+    const { address, lovelace, assets } = recipient
+    const setRecipient = (newRecipient: Recipient) => {
+      setRecipients(recipients.map((oldRecipient, _index) => {
+        return _index === index ? newRecipient : oldRecipient
+      }))
+    }
+    const setLovelace = (lovelace: bigint) => {
+      setRecipient({ ...recipient, lovelace })
+    }
+    const setAsset = (id: string, quantity: bigint) => {
+      setRecipient({
+        address,
+        lovelace,
+        assets: new Map(assets).set(id, quantity)
+      })
+    }
+
+    return (
+      <div key={index} className='p-4 my-2 rounded-md bg-white space-y-2'>
+        <label className='flex block border rounded-md overflow-hidden'>
+          <span className='p-2 bg-gray-200'>TO</span>
+          <input
+            className='p-2 block w-full outline-none'
+            value={address}
+            onChange={(e) => setRecipient({ ...recipient, address: e.target.value })}
+            placeholder='Address' />
+        </label>
+        <label className='flex block border rounded-md overflow-hidden'>
+          <CurrencyInput
+            className='p-2 block w-full outline-none'
+            decimals={6}
+            value={lovelace}
+            onChange={setLovelace}
+            placeholder='0.000000' />
+          <button>of&nbsp;{toADA(balance.lovelace)}</button>
+          <span className='p-2'>₳</span>
+        </label>
+        {Array.from(assets).map(([id, quantity]) => {
+          const max = balance.assets.get(id)
+          return (
+            <label key={id} className='flex block border rounded-md overflow-hidden'>
+              <CurrencyInput
+                decimals={0}
+                onChange={(value) => setAsset(id, value)}
+                className='p-2 block w-full outline-none'
+                value={quantity}
+              />
+              {max && <button>of&nbsp;{toDecimal(max, 0)}</button>}
+              <span className='p-2'>{getAssetName(id.slice(56))}</span>
+            </label>
+          )
+        })}
+        <div className='relative'>
+          <button className='block rounded-md bg-gray-200 p-2 peer'>Add Asset</button>
+          <ul className='absolute mt-1 divide-y bg-white text-sm max-h-64 rounded-md shadow overflow-y-scroll invisible peer-focus:visible hover:visible'>
+            {Array.from(balance.assets)
+              .filter(([id, _]) => !assets.has(id))
+              .map(([id, quantity]) => (
+                <li key={id}>
+                  <button
+                    onClick={() => setAsset(id, BigInt(0))}
+                    className='block w-full h-full px-1 py-2 hover:bg-slate-100'
+                  >
+                    <div className='flex space-x-2'>
+                      <span>{getAssetName(id.slice(56))}</span>
+                      <span className='grow text-right'>{quantity.toString()}</span>
+                    </div>
+                    <div className='flex space-x-1'>
+                      <span className='font-mono text-gray-500 text-xs'>{id.slice(0, 56)}</span>
+                    </div>
+                  </button>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    )
   }
 
-  if (balance) {
+  if (error) return <div>An error happened.</div>
+
+  if (loading) return (
+    <div className='text-center'>
+      Loading...
+    </div>
+  )
+
+  if (addressBalance) {
+    const balance = addressBalance
+
     return (
       <Layout>
-        <div className='p-4 rounded-md bg-white'>
+        <div className='p-4 rounded-md bg-white my-2'>
           <h1 className='font-medium text-center'>{address}</h1>
-          <h2 className='font-medium text-center text-lg'>{toDecimal(balance.lovelace, 6)}&nbsp;₳</h2>
-          <div className='space-y-2'>
-            <label className='flex block border rounded-md overflow-hidden'>
-              <span className='p-2 bg-gray-200'>TO</span>
-              <input className='p-2 block w-full outline-none' placeholder='Address' />
-            </label>
-            <label className='flex block border rounded-md overflow-hidden'>
-              <CurrencyInput
-                className='p-2 block w-full outline-none'
-                decimals={6}
-                value={lovelaceInput}
-                onChange={setLovelaceInput}
-                placeholder='0.000000' />
-              <span className='p-2'>₳</span>
-              <button className='p-2 bg-gray-200'>Max</button>
-            </label>
-            {assetInputs.map(({ id, quantity, max }) => (
-              <label key={id} className='flex block border rounded-md overflow-hidden'>
-                <CurrencyInput
-                  decimals={0}
-                  onChange={(value) => setAssetInput({ id, max, quantity: value })}
-                  className='p-2 block w-full outline-none'
-                  value={quantity}
-                />
-                <span className='p-2'>{getAssetName(id.slice(56))}</span>
-                <button className='p-2 bg-gray-200'>Max</button>
-              </label>
-            ))}
-            <div className='relative'>
-              <button className='block rounded-md bg-gray-200 p-2 peer'>Add Asset</button>
-              <ul className='absolute mt-1 divide-y bg-white text-sm max-h-64 rounded-md shadow overflow-y-scroll invisible peer-focus:visible hover:visible'>
-                {Array.from(balance.assets)
-                  .filter(([id, _]) => !assetInputs.find((asset) => asset.id === id))
-                  .map(([id, quantity]) => (
-                    <li key={id}>
-                      <button
-                        onClick={() => setAssetInputs(assetInputs.concat({ id, max: quantity, quantity }))}
-                        className='block w-full h-full px-1 py-2 hover:bg-slate-100'
-                      >
-                        <div className='flex space-x-2'>
-                          <span>{getAssetName(id.slice(56))}</span>
-                          <span className='grow text-right'>{quantity.toString()}</span>
-                        </div>
-                        <div className='flex space-x-1'>
-                          <span className='font-mono text-gray-500 text-xs'>{id.slice(0, 56)}</span>
-                        </div>
-                      </button>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </div>
+          <h2 className='font-medium text-center text-lg'>{toADA(addressBalance.lovelace)}&nbsp;₳</h2>
+        </div>
+        {recipients.map((recipient, index) => Recipient(recipient, index, balance))}
+        <div className='p-4 rounded-md bg-white my-2'>
+          <button
+            className='p-2 rounded-md bg-gray-200'
+            onClick={() => setRecipients(recipients.concat(defaultRecipient))}
+          >
+            Add Recipient
+          </button>
         </div>
       </Layout>
     )
