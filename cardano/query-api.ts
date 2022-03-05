30fa620eba2607ddbeb72941b2356add39c7be0d
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-03-05 14:33:09 +0800

    Replace switch-case to if

--- cardano/query-api.ts
+++ cardano/query-api.ts
@@ -68,50 +68,92 @@ const useAddressUTxOsQuery = (address: string, config: Config) => {
   useEffect(() => {
     let isMounted = true
 
-    switch (config.queryAPI.type) {
-      case 'graphql': {
-        const apollo = new ApolloClient({
-          uri: config.queryAPI.URI,
-          cache: new InMemoryCache()
-        })
+    if (config.queryAPI.type === 'graphql') {
+      const apollo = new ApolloClient({
+        uri: config.queryAPI.URI,
+        cache: new InMemoryCache()
+      })
 
-        type QueryData = {
-          utxos: {
-            txHash: string
-            index: number
-            value: string
-            tokens: {
-              asset: {
-                policyId: string
-                assetName: string
-              }
-              quantity: string
-            }[]
+      type QueryData = {
+        utxos: {
+          txHash: string
+          index: number
+          value: string
+          tokens: {
+            asset: {
+              policyId: string
+              assetName: string
+            }
+            quantity: string
           }[]
-        }
+        }[]
+      }
 
-        type QueryVars = {
-          address: string
-        }
+      type QueryVars = {
+        address: string
+      }
 
-        address && apollo.query<QueryData, QueryVars>({
-          query: UTxOsQuery,
-          variables: { address: address }
-        }).then(({ data }) => {
-          const utxos = data?.utxos
+      address && apollo.query<QueryData, QueryVars>({
+        query: UTxOsQuery,
+        variables: { address: address }
+      }).then(({ data }) => {
+        const utxos = data?.utxos
 
-          isMounted && utxos && setResult({
+        isMounted && utxos && setResult({
+          type: 'ok',
+          data: utxos.map((utxo) => {
+            return {
+              txHash: utxo.txHash,
+              index: utxo.index,
+              lovelace: BigInt(utxo.value),
+              assets: utxo.tokens.map(({ asset, quantity }) => {
+                return {
+                  policyId: asset.policyId,
+                  assetName: asset.assetName,
+                  quantity: BigInt(quantity)
+                }
+              })
+            }
+          })
+        })
+      }).catch(() => {
+        isMounted && setResult({ type: 'error' })
+      })
+    }
+
+    if (config.queryAPI.type === 'koios') {
+      const koios = createKoios(config)
+
+      address && koios.get('/api/v0/address_info', { params: { _address: address } })
+        .then(({ data }) => {
+          type Info = {
+            balance: string
+            stake_address: string
+            utxo_set: {
+              tx_hash: string
+              tx_index: number
+              value: string
+              asset_list: {
+                policy_id: string
+                asset_name: string
+                quantity: string
+              }[]
+            }[]
+          }
+          const info: Info = data?.[0]
+
+          isMounted && info && setResult({
             type: 'ok',
-            data: utxos.map((utxo) => {
+            data: info.utxo_set.map((utxo) => {
               return {
-                txHash: utxo.txHash,
-                index: utxo.index,
+                txHash: utxo.tx_hash,
+                index: utxo.tx_index,
                 lovelace: BigInt(utxo.value),
-                assets: utxo.tokens.map(({ asset, quantity }) => {
+                assets: utxo.asset_list.map((asset) => {
                   return {
-                    policyId: asset.policyId,
-                    assetName: asset.assetName,
-                    quantity: BigInt(quantity)
+                    policyId: asset.policy_id,
+                    assetName: asset.asset_name,
+                    quantity: BigInt(asset.quantity)
                   }
                 })
               }
@@ -120,50 +162,6 @@ const useAddressUTxOsQuery = (address: string, config: Config) => {
         }).catch(() => {
           isMounted && setResult({ type: 'error' })
         })
-      }
-
-      case 'koios': {
-        const koios = createKoios(config)
-
-        address && koios.get('/api/v0/address_info', { params: { _address: address } })
-          .then(({ data }) => {
-            type Info = {
-              balance: string
-              stake_address: string
-              utxo_set: {
-                tx_hash: string
-                tx_index: number
-                value: string
-                asset_list: {
-                  policy_id: string
-                  asset_name: string
-                  quantity: string
-                }[]
-              }[]
-            }
-            const info: Info = data?.[0]
-
-            isMounted && info && setResult({
-              type: 'ok',
-              data: info.utxo_set.map((utxo) => {
-                return {
-                  txHash: utxo.tx_hash,
-                  index: utxo.tx_index,
-                  lovelace: BigInt(utxo.value),
-                  assets: utxo.asset_list.map((asset) => {
-                    return {
-                      policyId: asset.policy_id,
-                      assetName: asset.asset_name,
-                      quantity: BigInt(asset.quantity)
-                    }
-                  })
-                }
-              })
-            })
-          }).catch(() => {
-            isMounted && setResult({ type: 'error' })
-          })
-      }
     }
 
     return () => {
@@ -207,91 +205,89 @@ const useProtocolParametersQuery = (config: Config) => {
   useEffect(() => {
     let isMounted = true
 
-    switch (config.queryAPI.type) {
-      case 'graphql': {
-        const apollo = new ApolloClient({
-          uri: config.queryAPI.URI,
-          cache: new InMemoryCache()
-        })
+    if (config.queryAPI.type === 'graphql') {
+      const apollo = new ApolloClient({
+        uri: config.queryAPI.URI,
+        cache: new InMemoryCache()
+      })
 
-        type QueryData = {
-          genesis: {
-            shelley: {
-              protocolParams: {
-                minFeeA: number
-                minFeeB: number
-                poolDeposit: number
-                keyDeposit: number
-                coinsPerUtxoWord: number
-                maxValSize: string
-                maxTxSize: number
-              }
+      type QueryData = {
+        genesis: {
+          shelley: {
+            protocolParams: {
+              minFeeA: number
+              minFeeB: number
+              poolDeposit: number
+              keyDeposit: number
+              coinsPerUtxoWord: number
+              maxValSize: string
+              maxTxSize: number
             }
           }
         }
+      }
 
-        apollo.query<QueryData>({ query: ProtocolParametersQuery }).then(({ data }) => {
-          const params = data?.genesis.shelley.protocolParams
+      apollo.query<QueryData>({ query: ProtocolParametersQuery }).then(({ data }) => {
+        const params = data?.genesis.shelley.protocolParams
 
+        params && isMounted && setResult({
+          type: 'ok',
+          data: {
+            minFeeA: params.minFeeA,
+            minFeeB: params.minFeeB,
+            poolDeposit: params.poolDeposit,
+            keyDeposit: params.keyDeposit,
+            coinsPerUtxoWord: params.coinsPerUtxoWord,
+            maxValSize: parseFloat(params.maxValSize),
+            maxTxSize: params.maxTxSize
+          }
+        })
+      }).catch(() => {
+        isMounted && setResult({ type: 'error' })
+      })
+    }
+
+    if (config.queryAPI.type === 'koios') {
+      const koios = createKoios(config)
+      koios.get('/api/v0/tip').then(({ data }) => {
+        type Tip = {
+          hash: string
+          epoch: number
+          abs_slot: number
+          epoch_slot: number
+          block_no: number
+          block_time: string
+        }
+        const tip: Tip = data?.[0]
+        tip && koios.get('/api/v0/epoch_params', { params: { _epoch_no: tip.epoch } }).then(({ data }) => {
+          type KoiosProtocolParameters = {
+            min_fee_a: number
+            min_fee_b: number
+            key_deposit: number
+            pool_deposit: number
+            coins_per_utxo_word: number
+            max_val_size: number
+            max_tx_size: number
+          }
+          const params: KoiosProtocolParameters = data?.[0]
           params && isMounted && setResult({
             type: 'ok',
             data: {
-              minFeeA: params.minFeeA,
-              minFeeB: params.minFeeB,
-              poolDeposit: params.poolDeposit,
-              keyDeposit: params.keyDeposit,
-              coinsPerUtxoWord: params.coinsPerUtxoWord,
-              maxValSize: parseFloat(params.maxValSize),
-              maxTxSize: params.maxTxSize
+              minFeeA: params.min_fee_a,
+              minFeeB: params.min_fee_b,
+              poolDeposit: params.pool_deposit,
+              keyDeposit: params.key_deposit,
+              coinsPerUtxoWord: params.coins_per_utxo_word,
+              maxValSize: params.max_val_size,
+              maxTxSize: params.max_tx_size
             }
           })
         }).catch(() => {
           isMounted && setResult({ type: 'error' })
         })
-      }
-
-      case 'koios': {
-        const koios = createKoios(config)
-        koios.get('/api/v0/tip').then(({ data }) => {
-          type Tip = {
-            hash: string
-            epoch: number
-            abs_slot: number
-            epoch_slot: number
-            block_no: number
-            block_time: string
-          }
-          const tip: Tip = data?.[0]
-          tip && koios.get('/api/v0/epoch_params', { params: { _epoch_no: tip.epoch } }).then(({ data }) => {
-            type KoiosProtocolParameters = {
-              min_fee_a: number
-              min_fee_b: number
-              key_deposit: number
-              pool_deposit: number
-              coins_per_utxo_word: number
-              max_val_size: number
-              max_tx_size: number
-            }
-            const params: KoiosProtocolParameters = data?.[0]
-            params && isMounted && setResult({
-              type: 'ok',
-              data: {
-                minFeeA: params.min_fee_a,
-                minFeeB: params.min_fee_b,
-                poolDeposit: params.pool_deposit,
-                keyDeposit: params.key_deposit,
-                coinsPerUtxoWord: params.coins_per_utxo_word,
-                maxValSize: params.max_val_size,
-                maxTxSize: params.max_tx_size
-              }
-            })
-          }).catch(() => {
-            isMounted && setResult({ type: 'error' })
-          })
-        }).catch(() => {
-          isMounted && setResult({ type: 'error' })
-        })
-      }
+      }).catch(() => {
+        isMounted && setResult({ type: 'error' })
+      })
     }
 
     return () => {
