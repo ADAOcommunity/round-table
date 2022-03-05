067903aee039986f5b2161e04904f0f7bf908aee
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-03-05 15:32:39 +0800

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ConfigContext, config } from '../cardano/config'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConfigContext.Provider value={[config, () => { }]}>
+      <Head>
+        <title>{config.isMainnet ? 'MultiSig (Mainnet)' : 'MultiSig (Testnet)'}</title>
+      </Head>
       <Component {...pageProps} />
     </ConfigContext.Provider>
   )
