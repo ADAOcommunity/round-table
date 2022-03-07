1c886073c4f9d9894d6879296ebfa149722996b4
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-03-07 19:30:09 +0800

    Use transaction in treasury

--- pages/treasuries/[treasury].tsx
+++ pages/treasuries/[treasury].tsx
@@ -5,27 +5,47 @@ import { getResult, useCardanoSerializationLib } from '../../cardano/serializati
 import { ErrorMessage, Loading } from '../../components/status'
 import { useContext } from 'react'
 import { ConfigContext } from '../../cardano/config'
+import { NewTransaction } from '../../components/transaction'
+import { useAddressUTxOsQuery, useProtocolParametersQuery } from '../../cardano/query-api'
 
-const NewProposal: NextPage = () => {
+const Treasury: NextPage = () => {
   const [config, _] = useContext(ConfigContext)
   const router = useRouter()
   const { treasury } = router.query
   const cardano = useCardanoSerializationLib()
+  const protocolParameters = useProtocolParametersQuery(config)
 
   if (!cardano) return <Loading />;
   if (typeof treasury !== 'string') return <ErrorMessage>Invalid script</ErrorMessage>;
-
   const parseResult = getResult(() => cardano.lib.NativeScript.from_bytes(Buffer.from(treasury, 'base64')))
   if (!parseResult.isOk) return <ErrorMessage>Invalid script</ErrorMessage>;
   const script = parseResult.data
   const address = cardano.getScriptAddress(script, config.isMainnet)
+  if (protocolParameters.type === 'loading') return <Loading />;
+  if (protocolParameters.type === 'error') return <ErrorMessage>An error happened when query protocol parameters.</ErrorMessage>;
+
+  const NewProposal = () => {
+    const utxos = useAddressUTxOsQuery(address.to_bech32(), config)
+    if (utxos.type === 'loading') return <Loading />;
+    if (utxos.type === 'error') return <ErrorMessage>An error happened when query balance.</ErrorMessage>;
+
+    return (
+      <NewTransaction
+        senderAddress={address}
+        cardano={cardano}
+        protocolParameters={protocolParameters.data}
+        previewURI={(body) => `/treasuries/${encodeURIComponent(treasury)}/${body}`}
+        utxos={utxos.data} />
+    )
+  }
 
   return (
     <Layout>
       <h1 className='my-8 font-bold text-2xl text-center'>Treasury - Proposal</h1>
       <h2 className='my-4 text-center'>{address.to_bech32()}</h2>
+      <NewProposal />
     </Layout>
   )
 }
 
-export default NewProposal
+export default Treasury
