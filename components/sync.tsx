7b83132642849e5314bcff7394b19f5d31afd7dd
rodrigomd94 <rodrigomd94@gmail.com>
2022-03-13 15:08:58 -0600

    fixed loadedSignatures

--- components/sync.tsx
+++ components/sync.tsx
@@ -16,10 +16,9 @@ const SyncToggle: NextPage<{
 
   useEffect(() => {
     if (toggled) {
-      console.log("activated")
+      var signatures: string[] = []
       gun.get(props.txHash).map().on((data) => {
-        var signatures: string[] = [...loadedSignatures];
-        console.log("changed")
+        signatures = [...loadedSignatures];
         //console.log(data)
         let sig: string = data.sig
         if (!signatures.includes(sig)) {
@@ -28,7 +27,6 @@ const SyncToggle: NextPage<{
           setLoadedSignatures(signatures)
         }
       })
-      console.log("here", signatures)
       props.signHandle(signatures)
     } else {
       console.log("deactivated")
@@ -36,7 +34,6 @@ const SyncToggle: NextPage<{
   }, [toggled])
 
   useEffect(() => {
-    console.log("sig map:", props.signatureMap)
     props.signatureMap.forEach((sig, hex) => {
       if (!loadedSignatures.includes(sig)) {
         gun.get(props.txHash).get(hex).put({ sig: sig })
