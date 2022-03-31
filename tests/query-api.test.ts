b48335b8abf5839ec2c8f88312c8ace31027a41b
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-04-01 07:56:04 +0800

    Move to src folder

--- tests/query-api.test.ts
+++ tests/query-api.test.ts
@@ -1,4 +1,4 @@
-import { getAssetName, getPolicyId } from '../cardano/query-api'
+import { getAssetName, getPolicyId } from '../src/cardano/query-api'
 
 const policyId = '126b8676446c84a5cd6e3259223b16a2314c5676b88ae1c1f8579a8f'
 const assetName = '7453554e444145'
