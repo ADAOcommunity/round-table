ae03b349953aafade18389975387710ad2112441
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-04-04 08:30:32 +0800

    Implement treasury deleting

--- src/pages/treasuries/[base64CBOR]/edit.tsx
+++ src/pages/treasuries/[base64CBOR]/edit.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { NextRouter, useRouter } from 'next/router'
 import { encodeCardanoData, getResult, useCardanoSerializationLib } from '../../../cardano/serialization-lib'
 import { Hero, Layout, Panel } from '../../../components/layout'
 import { ErrorMessage, Loading } from '../../../components/status'
@@ -10,11 +10,19 @@ import { db } from '../../../db'
 import { useEffect, useState } from 'react'
 
 const EditTreasury: NextPage<{
+  router: NextRouter
   script: NativeScript
-}> = ({ script }) => {
+}> = ({ router, script }) => {
   const treasury = useLiveQuery(async () => db.treasuries.get(encodeCardanoData(script, 'base64')), [script])
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
+
+  const deleteHandle = () => {
+    db
+      .treasuries
+      .delete(encodeCardanoData(script, 'base64'))
+      .then(() => router.push('/treasuries/new'))
+  }
 
   useEffect(() => {
     let isMounted = true
@@ -51,14 +59,18 @@ const EditTreasury: NextPage<{
           </textarea>
         </label>
       </div>
-      <footer className='flex justify-end p-4 bg-gray-100'>
-        <SaveTreasuryButton
-          className='px-4 py-2 bg-sky-700 text-white rounded shadow disabled:text-gray-400 disabled:bg-transparent'
-          name={name}
-          description={description}
-          script={script}>
-          Save Treasury
-        </SaveTreasuryButton>
+      <footer className='flex justify-between p-4 bg-gray-100'>
+        <button onClick={deleteHandle} className='px-4 py-2 text-sky-700'>Delete</button>
+        <div className='space-x-2'>
+          <button className='px-4 py-2 border rounded text-sky-700' onClick={() => router.back()}>Back</button>
+          <SaveTreasuryButton
+            className='px-4 py-2 bg-sky-700 text-white rounded shadow disabled:text-gray-400 disabled:bg-transparent'
+            name={name}
+            description={description}
+            script={script}>
+            Save Treasury
+          </SaveTreasuryButton>
+        </div>
       </footer>
     </Panel>
   )
@@ -80,12 +92,12 @@ const GetTreasury: NextPage = () => {
       <div className='space-y-2'>
         <Hero>
           <h1 className='font-semibold text-lg'>Edit Treasury Information</h1>
-          <p>Only descriptive information can be changed. If you want to change the signers, you have to create a new treasury.</p>
+          <p>Only descriptive information can be changed. If you want to change the signers, you have to create a new treasury. Deleting the info does not change any state of the treasury on chain. By deleting you merely deregister or forget it locally.</p>
         </Hero>
         <Panel>
           <NativeScriptViewer className='p-4 space-y-2' cardano={cardano} script={script} />
         </Panel>
-        <EditTreasury script={script} />
+        <EditTreasury router={router} script={script} />
       </div>
     </Layout>
   )
