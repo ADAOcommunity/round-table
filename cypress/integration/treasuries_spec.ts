81ce80b288f7f7b1dda5aec2e94d344de9c1e0a4
rodrigomd94 <rodrigomd94@gmail.com>
2022-04-06 07:36:05 -0600

    reformats the cypress code and separates fn to limit required signers

--- cypress/integration/treasuries_spec.ts
+++ cypress/integration/treasuries_spec.ts
@@ -1,14 +1,18 @@
 
-const treasuryName = "Test treasury 4"
+const treasuryName = "Test treasury"
 const treasuryDesc = "This is a description of the treasury"
-const addresses = ["addr_test1qrmtl76z2yvzw2zas03xze674r2yc6wefw0pm9v5x4ma6zs45zncsuzyfftj8x2ecg69z5f7x2f3uyz6c38uaeftsrdqms6z7t", "addr_test1qrsaj9wppjzqq9aa8yyg4qjs0vn32zjr36ysw7zzy9y3xztl9fadz30naflhmq653up3tkz275gh5npdejwjj23l0rdquxfsdj"]
+const addresses = [
+  "addr_test1qrmtl76z2yvzw2zas03xze674r2yc6wefw0pm9v5x4ma6zs45zncsuzyfftj8x2ecg69z5f7x2f3uyz6c38uaeftsrdqms6z7t",
+  "addr_test1qrsaj9wppjzqq9aa8yyg4qjs0vn32zjr36ysw7zzy9y3xztl9fadz30naflhmq653up3tkz275gh5npdejwjj23l0rdquxfsdj"
+]
+
 describe('Create a new treasury', () => {
   it('Should show new treasury form', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('New Treasury').click()
     cy.url().should('eq', 'http://localhost:3000/treasuries/new')
   })
-  it('should fill title and description', () => {
+  it('Should fill title and description', () => {
     cy.get('input[placeholder="Write Name"]')
       .type(treasuryName)
       .should("have.value", treasuryName);
@@ -18,7 +22,7 @@ describe('Create a new treasury', () => {
       .should("have.value", treasuryDesc)
   })
 
-  it('should add signers', () => {
+  it('Should add signers', () => {
     cy.contains('Add').should('be.disabled')
     var signers = 0
     addresses.map((address) => {
@@ -36,14 +40,14 @@ describe('Create a new treasury', () => {
   })
   //TODO: add test to check if add button is disabled when using wrong address
 
-  it('check if amount of required signers is limited to amount of signers added to treasury', () => {
+  it('Should limit required signers to amount of signers added to treasury', () => {
     cy.contains('Required Signers').parent().find('select').select('At least')
     cy.contains('Required Signers').parent().find('input').type('{selectall}{backspace}')
     cy.contains('Required Signers').parent().find('input').type('100')
     cy.contains('Required Signers').parent().find('input').should('have.value',addresses.length.toString())
   })
 
-  it('should save treasury', () => {
+  it('Should save treasury', () => {
     cy.contains('Required Signers').parent().find('input').type('{selectall}{backspace}')
     cy.contains('Required Signers').parent().find('input').type('2')
     cy.contains('Save Treasury').should('be.enabled')
