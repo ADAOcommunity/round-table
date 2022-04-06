c905948913c2a9fe2b9868f0f0d50bd5696f1ea4
rodrigomd94 <rodrigomd94@gmail.com>
2022-04-05 20:48:06 -0600

    fixes issue of detached element in cypress test
    added waiting time before clicking on saved treasury

--- cypress/integration/treasuries_spec.ts
+++ cypress/integration/treasuries_spec.ts
@@ -1,5 +1,5 @@
 
-const treasuryName = "Test treasury"
+const treasuryName = "Test treasury 4"
 const treasuryDesc = "This is a description of the treasury"
 const addresses = ["addr_test1qrmtl76z2yvzw2zas03xze674r2yc6wefw0pm9v5x4ma6zs45zncsuzyfftj8x2ecg69z5f7x2f3uyz6c38uaeftsrdqms6z7t", "addr_test1qrsaj9wppjzqq9aa8yyg4qjs0vn32zjr36ysw7zzy9y3xztl9fadz30naflhmq653up3tkz275gh5npdejwjj23l0rdquxfsdj"]
 describe('Create a new treasury', () => {
@@ -48,6 +48,7 @@ describe('Create a new treasury', () => {
     cy.contains('Required Signers').parent().find('input').type('2')
     cy.contains('Save Treasury').should('be.enabled')
     cy.contains('Save Treasury').click()
+    cy.wait(1500)
     cy.contains(treasuryName).click()
     cy.url().should('eq', 'http://localhost:3000/treasuries/gwMCgoIAWBz2v%2FtCURgnKF2D4mFnXqjUTGnZS54dlZQ1d90KggBYHOHZFcEMhAAXvTkIioJQeycVCkOOiQd4QiFJEwk%3D')
   })
