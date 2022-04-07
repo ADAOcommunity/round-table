d9c29c866d40488b4ca4035fe097c97d74fe646e
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-04-08 06:22:45 +0800

    Use keyHash as key

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,11 @@
 import Dexie, { Table } from 'dexie'
+import { Loader } from './cardano/multiplatform-lib'
 
 interface Treasury {
+  hash: string
   name: string
   description: string
-  script: string
+  script: Uint8Array
   updatedAt: Date
 }
 
@@ -36,6 +38,35 @@ class LocalDatabase extends Dexie {
 
     this.version(3).stores({
       treasuries: '&script',
+      treasuriesTemp: null
+    }).upgrade(async (transaction) => {
+      const treasuries = await transaction.table('treasuriesTemp').toArray()
+      await transaction.table('treasuries').bulkAdd(treasuries)
+    })
+
+    this.version(4).stores({
+      treasuries: null,
+      treasuriesTemp: '&hash'
+    }).upgrade(async (transaction) => {
+      const cardano = await Loader.load()
+      if (!cardano) throw new Error('Cannot load CML')
+      const treasuries = await transaction.table('treasuries').toArray().then((oldTreasuries) =>
+        oldTreasuries.map((treaury) => {
+          const script = Buffer.from(treaury.script, 'base64')
+          const nativeScript = cardano.lib.NativeScript.from_bytes(script)
+          const scriptHash = cardano.hashScript(nativeScript)
+          return {
+            ...treaury,
+            hash: scriptHash.to_hex(),
+            script
+          }
+        })
+      )
+      await transaction.table('treasuriesTemp').bulkAdd(treasuries)
+    })
+
+    this.version(5).stores({
+      treasuries: '&hash',
       treasuriesTemp: null
     }).upgrade(async (transaction) => {
       const treasuries = await transaction.table('treasuriesTemp').toArray()
