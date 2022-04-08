77db0fc6b9697b4b100137f6e2192ab50e6cb89b
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-04-09 00:58:38 +0800

    Improve config padding

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -9,36 +9,34 @@ const Configure: NextPage = () => {
 
   return (
     <Layout>
-      <Panel>
-        <div className='p-4 space-y-2'>
+      <Panel className='p-4 space-y-2'>
+        <p className='space-x-2'>
+          <span>Network:</span>
+          <span>{config.isMainnet ? 'Mainnet' : 'Testnet'}</span>
+        </p>
+        <p className='space-x-2'>
+          <span>Query API:</span>
+          <span>{config.queryAPI.type}</span>
+        </p>
+        {config.queryAPI.type == 'graphql' &&
           <p className='space-x-2'>
-            <span>Network:</span>
-            <span>{config.isMainnet ? 'Mainnet' : 'Testnet'}</span>
+            <span>Query URI:</span>
+            <span>{config.queryAPI.URI}</span>
           </p>
-          <p className='space-x-2'>
-            <span>Query API:</span>
-            <span>{config.queryAPI.type}</span>
-          </p>
-          {config.queryAPI.type == 'graphql' &&
-            <p className='space-x-2'>
-              <span>Query URI:</span>
-              <span>{config.queryAPI.URI}</span>
-            </p>
-          }
-          {config.gunPeers && <div>
-            <span>GUN Peers:</span>
-            <ul>
-              {config.gunPeers.map((peer, index) => <li key={index}>{peer}</li>)}
-            </ul>
-          </div>}
-        </div>
+        }
+        {config.gunPeers && <div>
+          <span>GUN Peers:</span>
+          <ul>
+            {config.gunPeers.map((peer, index) => <li key={index}>{peer}</li>)}
+          </ul>
+        </div>}
         <div className='font-semibold'>User Data Export/Import</div>
         <p>User data has to be on the same network. For example, data exported from testnet cannot be imported to mainnet.</p>
         <p>Save my treasuries</p>
         <div className='flex'>
           <ExportUserDataButton />
         </div>
-        <p className='mt-2'>Load my treasuries</p>
+        <p>Load my treasuries</p>
         <div className='flex'>
           <ImportUserData />
         </div>
