49d9a0e4820bd4a6f209f1acb94292130bafeb58
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-04-09 11:34:12 +0800

    Add more project info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,8 @@ const Home: NextPage = () => {
             <Image src='/logo.svg' width={200} height={260} alt='brand' />
           </div>
           <p>Round Table is ADAO Community’s open-source, multi-signature dApp on Cardano blockchain. It aims at making Multi-Sig easy, funny and comfortable. The project is designed and developed with decentralization in mind. All the libraries and tools were chosen in favor of decentralization. There is no server to keep your data. Your data is your own. It runs on your browser just like any other light wallets. You could also run it on your own PC easily.</p>
-          <p>Start your Multi-Sig journey by creating treasuries with your friends or family, or by <Link href='/config'><a className='text-sky-700'>importing your data</a></Link>.</p>
+          <p>Round Table supports Nami, Eternl/cc and Gero wallets to sign so far. Flint Experimental can be used as well. Start your Multi-Sig journey by creating treasuries with your friends or family, or by <Link href='/config'><a className='text-sky-700'>importing your data</a></Link>.</p>
+          <p>We have a nice community. If you have any issue or question, feel free to reach out to us on <a className='text-sky-700' target='_blank' rel='noreferrer' href='https://github.com/ADAOcommunity/round-table'>Github</a> or <a className='text-sky-700' target='_blank' rel='noreferrer' href='https://discord.gg/SDnm4GzY'>Discord</a>.</p>
         </Panel>
       </div>
     </Layout>
