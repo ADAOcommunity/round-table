5c6ebd46b958eb94e3cc2a8075fa00b315046f38
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-12-03 05:53:07 +0800

    Add Remove tab to personal wallet

--- src/pages/personal/[personalWalletId].tsx
+++ src/pages/personal/[personalWalletId].tsx
@@ -1,7 +1,7 @@
 import { useLiveQuery } from 'dexie-react-hooks'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useContext, useMemo, useState } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import type { FC } from 'react'
 import { CopyButton, Hero, Layout, Modal, Panel } from '../../components/layout'
 import { Loading } from '../../components/status'
@@ -11,6 +11,8 @@ import { useCardanoMultiplatformLib } from '../../cardano/multiplatform-lib'
 import type { AddressWithPaths } from '../../cardano/multiplatform-lib'
 import { ConfigContext } from '../../cardano/config'
 import { DocumentDuplicateIcon } from '@heroicons/react/24/solid'
+import { NotificationContext } from '../../components/notification'
+import { RemoveWallet } from '../../components/wallet'
 
 const AddressTable: FC<{
   addresses: AddressWithPaths[]
@@ -82,6 +84,61 @@ const Summary: FC = () => {
   )
 }
 
+const Edit: FC<{
+  wallet: PersonalWallet
+}> = ({ wallet }) => {
+  const [name, setName] = useState(wallet.name)
+  const [description, setDescription] = useState(wallet.description)
+  const { notify } = useContext(NotificationContext)
+
+  useEffect(() => {
+    setName(wallet.name)
+    setDescription(wallet.description)
+  }, [])
+
+  const canSave = name.length > 0
+
+  const save = () => {
+    db
+      .personalWallets
+      .update(wallet.id, { name, description })
+      .catch(() => notify('error', 'Failed to save'))
+  }
+
+  return (
+    <Panel>
+      <div className='p-4 space-y-4'>
+        <label className='block space-y-1'>
+          <div className="after:content-['*'] after:text-red-500">Name</div>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className='p-2 block border w-full rounded'
+            placeholder='Write Name' />
+        </label>
+        <label className='block space-y-1'>
+          <div>Description</div>
+          <textarea
+            className='p-2 block border w-full rounded'
+            placeholder='Describe the wallet'
+            rows={4}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}>
+          </textarea>
+        </label>
+      </div>
+      <footer className='flex justify-end p-4 bg-gray-100'>
+        <button
+          className='px-4 py-2 bg-sky-700 text-white rounded disabled:border disabled:text-gray-400 disabled:bg-gray-100'
+          disabled={!canSave}
+          onClick={save}>
+          Save
+        </button>
+      </footer>
+    </Panel>
+  )
+}
+
 const ShowPersonalWallet: NextPage = () => {
   const router = useRouter()
   const personalWallet = useLiveQuery(async () => {
@@ -89,11 +146,23 @@ const ShowPersonalWallet: NextPage = () => {
     if (typeof id !== 'string') return
     return db.personalWallets.get(parseInt(id))
   }, [router.query.personalWalletId])
-  const [tab, setTab] = useState<'summary' | 'receive' | 'spend' | 'multisig' | 'edit' | 'delete'>('summary')
+  const [tab, setTab] = useState<'summary' | 'receive' | 'spend' | 'multisig' | 'edit' | 'remove'>('summary')
+  const { notify } = useContext(NotificationContext)
 
   if (!personalWallet) return (
     <Modal><Loading /></Modal>
   )
+
+  const removeWallet = () => {
+    db
+      .personalWallets
+      .delete(personalWallet.id)
+      .then(() => router.push('/'))
+      .catch((error) => {
+        notify('error', 'Failed to delete')
+        console.error(error)
+      })
+  }
 
   return (
     <Layout>
@@ -120,6 +189,18 @@ const ShowPersonalWallet: NextPage = () => {
               className='px-2 py-1 disabled:bg-white disabled:text-sky-700'>
               Multisig
             </button>
+            <button
+              onClick={() => setTab('edit')}
+              disabled={tab === 'edit'}
+              className='px-2 py-1 disabled:bg-white disabled:text-sky-700'>
+              Edit
+            </button>
+            <button
+              onClick={() => setTab('remove')}
+              disabled={tab === 'remove'}
+              className='px-2 py-1 disabled:bg-white disabled:text-sky-700'>
+              Remove
+            </button>
           </nav>
         </div>
         {tab === 'multisig' && <div>These addresses are only for multisig. DO NOT USE THEM TO RECEIVE FUNDS.</div>}
@@ -127,6 +208,8 @@ const ShowPersonalWallet: NextPage = () => {
       {tab === 'summary' && <Summary />}
       {tab === 'receive' && <Receive wallet={personalWallet} />}
       {tab === 'multisig' && <Multisig wallet={personalWallet} />}
+      {tab === 'edit' && <Edit wallet={personalWallet} />}
+      {tab === 'remove' && <RemoveWallet walletName={personalWallet.name} remove={removeWallet} />}
     </Layout>
   )
 }
