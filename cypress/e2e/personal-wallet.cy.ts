cdd3651228188987e428f658945dc74a355c2076
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2022-12-09 14:12:35 +0800

    Test adding accounts

--- cypress/e2e/personal-wallet.cy.ts
+++ cypress/e2e/personal-wallet.cy.ts
@@ -1,19 +1,18 @@
 const walletName = 'Main Wallet'
+const password = 'ic{K6Bio"pMS'
+const walletDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+const recoveryPhrase: string[] = [
+  'moral',   'equip',   'attract',
+  'bacon',   'century', 'glad',
+  'frown',   'bottom',  'attitude',
+  'base',    'deputy',  'pink',
+  'erosion', 'beauty',  'razor',
+  'route',   'leave',   'wool',
+  'type',    'tell',    'trend',
+  'skin',    'weapon',  'blush'
+]
 
 describe('Personal wallet', () => {
-  const walletDesc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  const password = 'ic{K6Bio"pMS'
-  const recoveryPhrase: string[] = [
-    'moral',   'equip',    'attract',
-    'bacon',   'century',  'glad',
-    'frown',   'bottom',   'attitude',
-    'base',    'deputy',   'pink',
-    'erosion', 'beauty',   'razor',
-    'route',   'leave',    'wool',
-    'type',    'tell',     'trend',
-    'skin',    'weapon',   'blush'
-  ]
-
   it('should be able to recover with recovery phrase', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('New Wallet').click()
@@ -42,7 +41,7 @@ describe('Personal wallet', () => {
 
     cy.contains('Create').click()
 
-    cy.wait(10000)
+    cy.wait(15000)
 
     cy.contains('stake_test1uqhy9wspj5mhwz3jjw5sw7d8750mhqryg93xz562vkjwxpccdkfkl').should('be.visible')
 
@@ -86,6 +85,37 @@ describe('Personal wallet', () => {
       .should('have.text', "m/1854'/1815'/0'/0/1")
       .next('td')
       .should('have.text', "m/1854'/1815'/0'/2/1")
+  })
+
+  it('should be able to add personal accounts', () => {
+    cy.contains('Personal').click()
+    cy.contains('Add Account').click()
+    cy.get('input[type="Password"]').type(password)
+    cy.get('#modal-root').contains('Add Account').click()
+    cy.contains('Add Account').parent().get('select').select('1')
+
+    cy.contains('Receive').click()
+
+    cy.contains('addr_test1qz856plw0a560m23p5j6jwjj3sezjnrya0q6qjs7uezvrzqlcrjtpd2lkd088ka782nu8937fklr5lw75xs49wkhs6gsjyg4yw')
+      .closest('td')
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/0/0")
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/2/0")
+
+    cy.contains('addr_test1qz48vjvgvy72z8s9rthx3g64cpenp5g39r6glz3n98zcpkqlcrjtpd2lkd088ka782nu8937fklr5lw75xs49wkhs6gsu2lnfg')
+      .closest('td')
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/0/1")
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/2/0")
+
+    cy.contains('addr_test1qrfkagyyk6la02rvrfp7042h6d8e8a5j4depefrac3m94nqlcrjtpd2lkd088ka782nu8937fklr5lw75xs49wkhs6gsp9hpxw')
+      .closest('td')
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/0/2")
+      .next('td')
+      .should('have.text', "m/1852'/1815'/1'/2/0")
   })
 
   it('should be able to get removed', () => {
