7435d0c2dec1035fbd0db56aae5d695e7ad99b5c
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2023-01-26 15:56:09 +0800

    Add config modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ConfigContext, config, isMainnet } from '../cardano/config'
-import type { Config } from '../cardano/config'
 import Head from 'next/head'
 import { NotificationContext, useNotification } from '../components/notification'
 import { ApolloProvider } from '@apollo/client'
 import { createApolloClient } from '../cardano/query-api'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 
 const apolloClient = createApolloClient(config)
 
 function MyApp({ Component, pageProps }: AppProps) {
   const notification = useNotification()
   const title = useMemo(() => isMainnet(config) ? 'RoundTable' : `RoundTable ${config.network}`, [])
-  const configContext: [Config, () => void] = useMemo(() => [config, () => {}], [])
+  const configContext = useState(config)
 
   return (
     <ConfigContext.Provider value={configContext}>
